e28a57829e98d7fc8d8df03b14a41e274d73fdc1
ev-Zahri <zrimrnid@gmail.com>
2024-11-18 17:16:54 +0700

    Mengupdate folder

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ const serviceAccount = require("../firebaseServiceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://capstone-project-7f169.firebaseio.com"
+  databaseURL: "https://capstone-project-7f169.firebaseio.com",
+  // storageBucket: "capstone-project-b06a2.firebasestorage.app",
 });
 
 const db = admin.firestore();
