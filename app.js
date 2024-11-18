47d9e2cb3060c836533f9bc5b4e012f249d443cf
Zharii ini <125188722+ev-Zahri@users.noreply.github.com>
2024-11-18 12:40:32 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
 
-// Connect ke MongoDB
-connectDB();
-
 // Middleware untuk parsing JSON
 app.use(express.json());
 
-// Rute Auth
+// Rute AuthRoutes
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 module.exports = app;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,21 +1,8 @@
 const express = require('express');
+const { registerUser, loginUser } = require('../controllers/authController');
 const router = express.Router();
-const {
-    register,
-    login,
-    getUserProfile,
-    updateUserProfile,
-    deleteUser
-} = require('../controllers/authController');
-const authenticateToken = require('../middleware/authMiddleware');
 
-// Rute untuk registrasi dan login (tidak perlu autentikasi)
-router.post('/register', register);
-router.post('/login', login);
-
-// Rute untuk profil user (butuh autentikasi)
-router.get('/profile', authenticateToken, getUserProfile);
-router.put('/profile', authenticateToken, updateUserProfile);
-router.delete('/profile', authenticateToken, deleteUser);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
