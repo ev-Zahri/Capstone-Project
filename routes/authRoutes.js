4ee68a1dc1dda8cf1fb66bbd09cc26562a640287
DeAlNu <129713355+DeAlNu@users.noreply.github.com>
2024-11-19 17:52:38 +0700

    Penambahan User Management

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController');
+const { getUserById, updateUserById, deleteUserById } = require('../controllers/userController');
+const authenticateToken = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/:userId', authenticateToken, getUserById);
+router.put('/:userId', authenticateToken, updateUserById);
+router.delete('/:userId', authenticateToken, deleteUserById);
 
 module.exports = router;
